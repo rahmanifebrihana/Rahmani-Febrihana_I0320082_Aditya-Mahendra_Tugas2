c0b631cda0a454ade351070ab014bac0322c8e65
Rahmani Febrihana M.S <rfebrihana@gmail.com>
2021-03-12 00:23:42 -0800

    pengerjaan tugas

---
+++ I0320082_soal2_tugas2.py
@@ -0,0 +1,33 @@
+Nama_lengkap = "Rahmani Febrihana Mustari Samberbori"
+Nama_panggilan = "Hana"
+Tempat_lahir = "Jayapura"
+Tanggal_sekarang = 12
+Bulan_sekarang = 3
+Tahun_sekarang = 2021
+Tanggal_lahir = 17
+Bulan_lahir = 7
+Tahun_lahir = 2002
+Umur_bulan = ((Tanggal_sekarang - Tanggal_lahir)/30 + (Bulan_sekarang - Bulan_lahir) + (Tahun_sekarang - Tahun_lahir)*12)
+Perguruan_tinggi = "Universitas Sebelas Maret"
+Prodi = "S-1 Teknik Industri"
+Fakultas = "Teknik"
+Angkatan = 2020
+Jalur_masuk = "SNMPTN"
+Jenis_kelamin = "Perempuan"
+Golongan_darah = "0"
+Jumlah_saudara = "dua"
+Agama = "Islam"
+Alamat_rumah = "Jaya asri blok AF 16 Entrop, Jayapura-Papua"
+Kode_pos = 99224
+Hobi ="menonton drama korea"
+Cita_cita ="bekerja di salah satu perusahaan BUMN"
+Berat_badan = "64.5 kg"
+Tinggi_badan = "163 cm"
+Email = "rfebrihana@gmail.com"
+Instagram = "hannafbr"
+Makanan_favorite = "sate, bakso, tempe"
+Minuman_favorite = "es cream, es teh"
+Warna_favorite = "hitam, putih, abu_abu"
+print("Hallo namaku",Nama_lengkap,"\n","biasa di panggil",Nama_panggilan,"\n","saya lahir di",Tempat_lahir,"\n","umur saya",Umur_bulan,"bulan","\n","saya kuliah di",Perguruan_tinggi,"\n","prodi",Prodi,"\n","fakultas",Fakultas,"\n","saya angkatan",Angkatan,"\n","saya masuk jalur",Jalur_masuk,"\n","jenis kelamin saya",Jenis_kelamin,"\n","golongan darah saya",Golongan_darah,"\n","agama saya",Agama,"\n","alamat rumah saya di",Alamat_rumah,"\n","kode pos kelurahan saya",Kode_pos,"\n","hobi saya",Hobi,"\n","saya mempunyai cita cita",Cita_cita,"\n","berat badan saya",Berat_badan,"\n","dan tinggi badan saya",Tinggi_badan,"\n","alamat email saya",Email,"\n","akun instagram saya",Instagram,"\n","makanan favorite saya",Makanan_favorite,"\n","dan minuman favorite saya",Minuman_favorite,"\n","yang terakhir warna favorite saya",Warna_favorite,"\n")
+
+
