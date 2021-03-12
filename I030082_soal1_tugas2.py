016b4ba0dc87569036418ec76b9090fa1b352acf
Rahmani Febrihana M.S <rfebrihana@gmail.com>
2021-03-11 22:47:58 -0800

    pengerjaan tugas

---
+++ I030082_soal1_tugas2.py
@@ -0,0 +1,21 @@
+panjang = float(input("Memasukan panjang"))
+lebar = float(input("Memasukan lebar"))
+luas = panjang*lebar
+print("Luas persegi panjang adalah", luas, "satuan")
+
+phi = 3.14
+jari_jari =float(input("Memasukkan jari_jari"))
+luas = jari_jari*jari_jari*phi
+print("Luas lingkaran adalah", luas, "satuan")
+
+sisi = float(input("Masukan sisi"))
+luas_permukaan = 6*sisi*sisi
+print("Luas permukaan kubus adalah", luas_permukaan, "satuan")
+
+celcius = float(input("Masukan suhu celcius"))
+konversi =(((9/5)*celcius)+32)
+print("Konversi suhu dari celcius ke fahrenheit adalah", konversi, "derajat")
+
+reamur =float(input("Memasukkan suhu reamur"))
+konversi = ((5/4)*reamur)+273
+print("Konversi suhu dari reamur ke kelvin adalah", konversi, "derajat")
